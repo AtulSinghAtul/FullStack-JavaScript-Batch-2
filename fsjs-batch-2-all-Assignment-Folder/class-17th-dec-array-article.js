01f47c94968a5f7d3bdae2e64e957fbfe1a49088
Atul Singh <atulsinghsrj@gmail.com>
2022-12-21 13:34:38 +0530

    update articles code

--- fsjs-batch-2-all-Assignment-Folder/class-17th-dec-array-article.js
+++ fsjs-batch-2-all-Assignment-Folder/class-17th-dec-array-article.js
@@ -159,16 +159,87 @@
 //console.log(typeof var1); //string
 
 //lastIndexOf Method
-let arr = [1, 2, 3, 4, "atul", 5, 6, "atul", 7, 8, "atul"];
+//let arr = [1, 2, 3, 4, "atul", 5, 6, "atul", 7, 8, "atul"];
 //start searches the value from last index because last index of start searches from last indexes
-console.log(arr.lastIndexOf("atul")); // 10
+//console.log(arr.lastIndexOf("atul")); // 10
 
 //lastIndexOf() with two Parameters
 
 // second argument specifies the starting index
 // from where the method searches the element backward
-console.log(arr.lastIndexOf("atul", 8)); // 7
+//console.log(arr.lastIndexOf("atul", 8)); // 7
 
 // lastIndexOf() with Negative Parameter
 // start the searches from last
-console.log(arr.lastIndexOf("atul", -5)); //4
+//console.log(arr.lastIndexOf("atul", -5)); //4
+
+//Map() Method
+
+// let num = [1, 4, 9, 16, 25];
+
+// let mapSqrt = num.map(Math.sqrt);
+// console.log(mapSqrt);//[1,2,3,4,5]
+// console.log(num);//[1, 4, 9, 16, 25]
+
+// pop() Method
+// let num = [1, 4, 9, 16, 25];
+// let popMethod = num.pop();
+// console.log(popMethod);//25
+// console.log(num);//[ 1, 4, 9, 16 ]
+
+//reverse Method
+// let num = [1, 4, 9, 16, 25];
+// console.log(num.reverse());
+// console.log(num);
+
+//shift() method
+//let num = [1, 4, 9, 16, 25];
+//shift element from the first position in array and this will change all index position
+//console.log(num.shift()); // 1
+//modifing the original array
+//console.log(num); //[ 4, 9, 16, 25 ]
+
+// unshift() Method
+// let num = [1, 4, 9, 16, 25];
+// console.log(num.unshift(111, 222, 333, 444));
+// console.log(num);
+
+//sort() Method
+// let city = ["California", "Barcelona", "Paris", "Kathmandu"];
+// sort the city array in ascending order
+//let sortedArray = city.sort();
+//console.log(sortedArray); //[ 'Barcelona', 'California', 'Kathmandu', 'Paris' ]
+//console.log(city); //[ 'Barcelona', 'California', 'Kathmandu', 'Paris' ]
+
+//let num = [21, 6, 98, 176, 3, 1];
+//console.log(num.sort()); //[ 1, 176, 21, 3, 6, 98 ]
+//console.log(num); //[ 1, 176, 21, 3, 6, 98 ]
+
+// when compare function is passed
+// let ascend = city.sort(function (a, b) {
+//   return a.length - b.length;
+// });
+//console.log(ascend); //[ 'Paris', 'Barcelona', 'Kathmandu', 'California' ]
+//console.log(city); //[ 'Paris', 'Barcelona', 'Kathmandu', 'California' ]
+
+//ascending and descending order argest using function parameter
+//let num = [45, 7, 8, 67, 0, 8, 4, 343, 21];
+
+// let numAsc = num.sort(function name(a, b) {
+//   return a - b;
+// });
+//ascending order
+//console.log(numAsc); //[ 0,  4,  7,   8, 8, 21, 45, 67, 343]
+
+//change original array
+//console.log(num); //[ 0,  4,  7,   8, 8, 21, 45, 67, 343]
+
+// let numAsc1 = num.sort(function name(a, b) {
+//   return b - a;
+// });
+//descending order
+//console.log(numAsc); //[343, 67, 45, 21, 8, 8, 7, 4, 0]
+//change original array
+//console.log(num); //[343, 67, 45, 21, 8, 8, 7, 4, 0]
+
+//split() Method
