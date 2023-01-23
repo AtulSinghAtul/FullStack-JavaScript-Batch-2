a5940b25b6d299ef63c36a9932e81fb4fd052558
Atul Singh <atulsinghsrj@gmail.com>
2023-01-23 15:07:01 +0530

    add class dome home work

--- fsjs-batch-2-all-Assignment-Folder/class-21th-jan-dom-assignment/dom.js
+++ fsjs-batch-2-all-Assignment-Folder/class-21th-jan-dom-assignment/dom.js
@@ -1,1 +1,22 @@
-document.body.style.backgroundColor = "yellow";
+let bodyMain = document.querySelector("body");
+let clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear Circle";
+clearButton.classList.add("clear-button");
+bodyMain.appendChild(clearButton);
+
+bodyMain.addEventListener("click", function (e) {
+  let element = document.createElement("div");
+  element.classList.add("circle");
+
+  // console.log(element);
+  element.style.left = e.clientX + "px";
+  element.style.top = e.clientY + "px";
+
+  element.style.borderRadius = "50%";
+  console.log(bodyMain);
+  bodyMain.appendChild(element);
+});
+
+clearButton.addEventListener("click", function () {
+  document.location.reload();
+});
